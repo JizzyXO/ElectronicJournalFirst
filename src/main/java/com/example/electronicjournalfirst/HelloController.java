c249080e02a6fc2bbfa23ac5bb96b773d166f6fd
DasAdErt <misha_tkachev_2018@mail.ru>
2022-12-21 12:14:49 +0300

    Create interface login

--- src/main/java/com/example/electronicjournalfirst/HelloController.java
+++ src/main/java/com/example/electronicjournalfirst/HelloController.java
@@ -2,13 +2,24 @@ package com.example.electronicjournalfirst;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private PasswordField idField;
+
+    @FXML
     protected void loginMessage() {
-        welcomeText.setText("Вход выполнен!");
+        checkId();
+
+        //обработка запроса и вывод электронного журнала по заданному айди
+    }
+
+    @FXML
+    protected void checkId() {
+        //Проверка айди на валидность и прочее, возврат результата
     }
 }
---
+++ src/main/java/com/example/electronicjournalfirst/JournalController.java
@@ -0,0 +1,5 @@
+package com.example.electronicjournalfirst;
+
+public class JournalController {
+
+}
