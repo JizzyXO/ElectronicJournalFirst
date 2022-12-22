eceef9ca9bdfa08d5f470d47a239af764a6051d8
DasAdErt <misha_tkachev_2018@mail.ru>
2022-12-22 14:27:53 +0300

    ElectronicJournal

--- src/main/java/com/example/electronicjournalfirst/HelloApplication.java
+++ src/main/java/com/example/electronicjournalfirst/HelloApplication.java
@@ -5,14 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Journal.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1400, 720);
-        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/electronicjournalfirst/JournalController.java
+++ src/main/java/com/example/electronicjournalfirst/JournalController.java
@@ -1,5 +1,17 @@
 package com.example.electronicjournalfirst;
 
+import Backend.ElectronicJournal;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
 public class JournalController {
 
 }
